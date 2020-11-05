241dd5cdeeea4fcd0f05f5ea888d89988a7b9818
Cristina Nguyen <c.nguyen0994@gmail.com>
2020-11-05 01:47:03 -0800

    add all skill imgs to Skills component

--- src/App.js
+++ src/App.js
@@ -1,29 +1,27 @@
 import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
-import { Route, Switch as Switch2 } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from "react-router-dom";
 import About from "./components/About"
+import Skills from "./components/Skills"
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <p>
-          Testing
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>
+          Welcome to my portfolio!
+        </h1>
       </header>
-      <Switch2>
-        <Route path='/about' component={About} />
-      </Switch2>
+      <Router>
+          <Route path='/about' component={About} />
+          <Route path='/skills' component={Skills} />
+      </Router>
+     
     </div>
   );
 }
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,9 +1,27 @@
-import React from "react";
+import React, {Component} from 'react';
 
-const Skills = () => {
-    return (
-        <div></div>
-    )
+export default class Skills extends Component {
+    render() {
+        function importAll(r) {            
+            return r.keys().map(r);
+          }
+        const images = importAll(require.context('../assets/skills', false, /\.(png|jpe?g|svg)$/));
+        // console.log(images);
+        // console.log(images[0].default)
+
+        return (
+            <div id="skills">
+                <h1 className="heading">Technical Skills</h1>
+                <div className="line"></div>
+                {/* <img src={images[0].default}></img>  */}
+                <div>
+                    {images.map(e => (
+                        <li>
+                            <img src={e.default}></img>
+                        </li>
+                    ))}
+                </div>           
+            </div>
+        )
+    }
 }
-
-export default Skills;
