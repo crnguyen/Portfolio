a06526065da6056f4120ee4f65b81da5858435e2
Cristina Nguyen <c.nguyen0994@gmail.com>
2020-11-08 23:08:32 -0800

    add more contributions

--- src/components/Contributions.js
+++ src/components/Contributions.js
@@ -5,8 +5,13 @@ export default function Contributions () {
         <div id="Contributions" className="sections">
             <h1>Open Source Contributions</h1>
             <p>A collection of github projects/sites that I'm working on with others.</p>
-            <p>sButtons</p>
-            <a href="https://sbuttons.github.io/sbuttons/" class="button">Site</a>
+            <div>
+                <p>sButtons</p>
+                <a href="https://sbuttons.github.io/sbuttons/" class="button">Site</a>
+                <p>Python For Beginners</p>
+                <a href="https://github.com/crnguyen/Python-for-beginner" class="button">Site</a>
+
+            </div>
         </div>
     )
 }
