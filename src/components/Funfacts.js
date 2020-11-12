e2efa8ddf09c59e87ae731c562be749b4b2a3da2
Cristina Nguyen <c.nguyen0994@gmail.com>
2020-11-12 12:29:54 -0800

    deploy to firebase

--- src/components/Funfacts.js
+++ src/components/Funfacts.js
@@ -5,11 +5,18 @@ export default function Funfacts() {
     return (
         <div id="Funfacts" className="sections">
             <h1 className="titleDecoration">fun facts/interests</h1>
-            <div className="allProjects">
-                <div className="container">
-                    <img alt="kona" className="funFactPics" src={Kona}></img>
+
+            <div class="funFactContent">
+                <img alt="kona" className="funFactPics" src={Kona}></img>
+                <div class="funFactText">
+                    <h3><strong>I have a 1 year old corgi/aussie mix named Kona!</strong></h3>
                 </div>
-                I have a 1 year old corgi/aussie mix named Kona
+            </div>
+            <div class="funFactContent">
+                <div class="funFactText">
+                    <h3><strong>I have a 1 year old corgi/aussie mix named Kona!</strong></h3>
+                </div>
+                <img alt="kona" className="funFactPics" src={Kona}></img>
             </div>
         </div>
     )
