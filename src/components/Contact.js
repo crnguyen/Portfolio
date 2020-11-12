12246962cd4abe2b1de83ee867c6983e23795d75
Cristina Nguyen <c.nguyen0994@gmail.com>
2020-11-12 13:31:42 -0800

    add embedded Calendly to site

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,6 +4,9 @@ export default function Contact () {
     return (
         <div id="Contact" className="sections">
             <h1 className="titleDecoration">Contact me</h1>
+            <a href="mailto:c.nguyen0994@gmail.com" target="_top">Send an Email</a>
+            {/* <!-- Calendly inline widget begin --> */}
+            <div class="calendly-inline-widget" data-url="https://calendly.com/cnguyen0994/60min" style={{minWidth: 320, height: 530}}></div>
         </div>
     )
 }
