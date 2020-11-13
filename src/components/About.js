e3bd9d8b0b9116556722b86d6722c0e33f1b2d9b
Cristina Nguyen <c.nguyen0994@gmail.com>
2020-11-12 21:52:59 -0800

    add in media queries for large desktop

--- src/components/About.js
+++ src/components/About.js
@@ -7,12 +7,14 @@ export default function About () {
     });
         return (
             <div id="About" className="sections aos-item">
-                <h1 className="aboutOpening1" data-aos="fade-right" data-aos-easing="ease-in-sine">Hello, World!</h1>
-                <h2 className="aboutOpening2" data-aos="fade-left" data-aos-easing="ease-in-sine" 
-                data-aos-delay="2000">My name is Cristina and I'm a <b className="coloredText">
-                    solutions-driven full stack developer</b> based in the San Francisco Bay Area.</h2><h2 className="aboutOpening2" data-aos="fade-right" data-aos-easing="ease-in-sine" 
-                data-aos-delay="2000">I have a passion for <b className="coloredText">responsive 
-                and intuitive design on both client-side and server-side</b>, and can help you create a variety of digital products.</h2>
+                
+                    <h1 className="aboutOpening1" data-aos="fade-right" data-aos-easing="ease-in-sine">Hello, World!</h1>
+                <div className="aboutSection">
+                    <h2 className="aboutOpening2" data-aos="fade-left" data-aos-easing="ease-in-sine" 
+                    data-aos-delay="2000">My name is Cristina and I'm a <b className="coloredText">
+                        solutions-driven full stack developer</b> based in the San Francisco Bay Area. I have a passion for <b className="coloredText">responsive 
+                    and intuitive design on both client-side and server-side</b>, and can help you create a variety of digital products.</h2>
+                </div>
             </div>
         )
 }
