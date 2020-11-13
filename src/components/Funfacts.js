d876b9bd2d33c6df093f8122b72fede16ae751ff
Cristina Nguyen <c.nguyen0994@gmail.com>
2020-11-12 16:03:33 -0800

    center, align, and word wrap text and images

--- src/components/Funfacts.js
+++ src/components/Funfacts.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Kona from "../assets/funfacts/Kona.png"
+import Travel from "../assets/funfacts/travel.png"
 
 export default function Funfacts() {
     return (
@@ -8,17 +9,18 @@ export default function Funfacts() {
 
             <div class="funFactContent">
                 <img alt="kona" className="funFactPics" src={Kona}></img>
-                
-                <span><h3><strong>I have a 1 year old corgi/aussie mix named Kona!</strong></h3>
+                <div class="funFactText">
+                <span><h3><strong>I have a 1 year old puppy named Kona! He is 75% corgi and 25% australian shepherd. </strong></h3>
                     </span>
+                </div>
                 
             </div>
 
             <div class="funFactContent">
                 <div class="funFactText">
-                    <h3><strong>I have a 1 year old corgi/aussie mix named Kona!</strong></h3>
+                    <h3><strong>I love traveling! Pre-pandemic, I have traveled to Hawaii, New Orleans, Oregon, and Washington. In the future, I hope to visit other countries.</strong></h3>
                 </div>
-                <img alt="kona" className="funFactPics" src={Kona}></img>
+                <img alt="travel" className="funFactPics" src={Travel}></img>
             </div>
         </div>
     )
