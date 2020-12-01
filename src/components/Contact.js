1c0cd062831905818885c41ab4f4326094201903
Cristina Nguyen <c.nguyen0994@gmail.com>
2020-11-30 20:25:23 -0800

    fix calendly widget

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,7 +6,7 @@ export default function Contact () {
             <h1 className="titleDecoration">Contact me</h1>
             <a href="mailto:c.nguyen0994@gmail.com" className="linkButton" target="_top">Send an Email</a>
             <div className="calendlyContainer">
-                <div className="calendly-inline-widget" data-url="https://calendly.com/cnguyen0994/60min" style={{minWidth: 320, height: 530}}></div>
+                <div className="calendly-inline-widget" data-url="https://calendly.com/cnguyen0994" style={{minWidth: 320, height: 530}}></div>
             </div>
         </div>
     )
