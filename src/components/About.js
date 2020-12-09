7366e712882885fea7dd7614286a008eb50b8f92
Cristina Nguyen <c.nguyen0994@gmail.com>
2020-12-08 22:33:42 -0800

    add in another contribution

--- src/components/About.js
+++ src/components/About.js
@@ -10,9 +10,10 @@ export default function About () {
                 <h1 className="aboutOpening1" data-aos="fade-right" data-aos-easing="ease-in-sine">Hello, World!</h1>
                 <div className="aboutSection">
                     <h2 className="aboutOpening2" data-aos="fade-left" data-aos-easing="ease-in-sine" 
-                    data-aos-delay="2000">My name is Cristina and I'm a <b className="coloredText">
+                        data-aos-delay="2000">My name is Cristina and I'm a <b className="coloredText">
                         solutions-driven full stack developer</b> based in the San Francisco Bay Area. I have a passion for <b className="coloredText">responsive 
-                    and intuitive design on both client-side and server-side</b>, and can help you create a variety of digital products.</h2>
+                        and intuitive design on both client-side and server-side</b>, and can help you create a variety of digital products.
+                    </h2>
                 </div>
             </div>
         )
--- src/components/Contributions.js
+++ src/components/Contributions.js
@@ -1,12 +1,12 @@
 import React from "react";
 import Button from '@material-ui/core/Button';
 import Sbuttons from "../assets/contributions/sButtons.png"
-// import ehSpook from "../assets/contributions/ehs.png"
+import ehBiz from "../assets/contributions/eh.png"
 
 export default function Contributions () {
     return (
         <div id="Contributions" className="sections">
-            <h1 className="titleDecoration">Open Source Contributions</h1>
+            <h1 className="titleDecoration">Projects/Open Source Contributions</h1>
             <h4>A collection of github projects/sites that I'm working on with others.</h4>
 
             <div className="allProjects">
@@ -16,6 +16,14 @@ export default function Contributions () {
                         <div className="projectTitles">sButtons</div>
                         <Button className="projectButtons" variant="outlined" color="inherit" href="https://sbuttons.github.io/sbuttons/">Visit Site</Button>
                         <div className="textDetails">Contributed to adding new styles of buttons and cleaning up code to make it more readable.</div>
+                    </div>
+                </div>
+                <div className="container">
+                    <img alt="ehbiz" className="projectPics" src={ehBiz}></img>
+                    <div className="centered overlay overlayRight">
+                        <div className="projectTitles">ehBizOps</div>
+                        <Button className="projectButtons" variant="outlined" color="inherit" href="https://ehbizops.web.app/">Visit Site</Button>
+                        <div className="textDetails">Contributed to creating a React app for business management.</div>
                     </div>
                 </div>
 
