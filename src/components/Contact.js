8ceee0ca6c848b5d020d5f597b506028231f487d
Cristina Nguyen <c.nguyen0994@gmail.com>
2020-12-12 00:31:10 -0800

    delete buttons, add social media icons

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,12 +1,15 @@
 import React from "react";
+import { SocialIcon } from 'react-social-icons';
 
 export default function Contact () {
     return (
         <div id="Contact" className="sections">
             <h1 className="titleDecoration">Contact me</h1>
-            <a href="mailto:c.nguyen0994@gmail.com" className="linkButton" target="_top">Email</a>
-            <a href="https://www.linkedin.com/in/cristinanguyen/" className="linkButton" target="_top">LinkedIn</a>
-            <a href="https://github.com/crnguyen" className="linkButton" target="_top">Github</a>
+            
+            <SocialIcon className="socialIcons" url="mailto:c.nguyen0994@gmail.com" network="email" bgColor="black" />
+            <SocialIcon className="socialIcons" url="https://www.linkedin.com/in/cristinanguyen/" network="linkedin" bgColor="black" />
+            <SocialIcon className="socialIcons" url="https://github.com/crnguyen" network="github" bgColor="black" />
+            
             <div className="calendlyContainer">
                 <div className="calendly-inline-widget" data-url="https://calendly.com/cnguyen0994" style={{minWidth: 320, height: 530}}></div>
             </div>
